2dda72d282708e11de6fb318ec610802edfe3e32
Ondairos <ondairos@gmail.com>
2022-10-20 18:22:48 +0300

    added comments to app.js file

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const path = require('path');
 const helper = require('./helper'); //helper js file
 
+//env variables for secure mongoose connect
 const usernamedb = process.env.DB_USERNAME;
 const passdb = process.env.DB_PASSWORD;
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//package includes
+const express = require('express');  //framework
+const bodyParser = require('body-parser'); //parser
+const mongoose = require('mongoose'); //database ODM object document mapping library
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require('./controllers/error'); //404 controller
+const User = require('./models/user'); //user model
+ 
+const app = express();  //main express init
 
-const app = express();
-
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');  //EJS as the template engine
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));   //bodyparser config
+app.use(express.static(path.join(__dirname, 'public'))); // path config with express
 
 app.use((req, res, next) => {
   User.findById('635151936812fda3e75f8764')
@@ -31,11 +33,12 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes); //web app routes for admin
+app.use(shopRoutes); //shop routes for users
 
-app.use(errorController.get404);
+app.use(errorController.get404); //use of 404 controller
 
+//mongoose init and app init
 mongoose.connect(`mongodb+srv://${usernamedb}:${passdb}@cluster0.ga01wzx.mongodb.net/shop?retryWrites=true&w=majority`)
   .then(result => {
     User.findOne().then(user => {
