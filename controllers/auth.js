6e1fdf4e89abd54643c56198e30407a52b7b659b
Ondairos <ondairos@gmail.com>
2022-10-21 12:48:38 +0300

    added cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1];
+    console.log(isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     });
+};
+
+exports.postLogin = (req, res, next) => {
+    //cookie
+    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.redirect('/');
 };
