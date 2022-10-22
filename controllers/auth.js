7939d5a69187517da5fd47f23f89fa4b3e69ab44
Ondairos <ondairos@gmail.com>
2022-10-22 12:53:26 +0300

    added signup ejs and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,39 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: false
-    });
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-    //session
-    User.findById('635151936812fda3e75f8764')
-        .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user; //share across requests
-            req.session.save(err => {  //redirect after the session is created in the mongodb and not before
-                console.log(err);
-                res.redirect('/');
-            });
-        })
-        .catch(err => console.log(err));
-};
-
-exports.postLogout = (req, res, next) => {
-    req.session.destroy((err) => {
+  User.findById('635151936812fda3e75f8764')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
-    }); //method of the package mongodb-session
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
