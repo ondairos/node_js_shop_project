d1f923610e290c09780a05ec98f9325ecbccf45b
Ondairos <ondairos@gmail.com>
2022-10-22 16:31:38 +0300

    added flash error mesage to signup pages and css

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ exports.getLogin = (req, res, next) => {
   if (message.length > 0) {
     message = message[0];
   } else {
-    message =null;
+    message = null;
   }
   res.render('auth/login', {
     path: '/login',
@@ -16,9 +16,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message
   });
 };
 
@@ -42,6 +49,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid email or password.'); //flash message
           res.redirect('/login');
         })
         .catch(err => {
@@ -60,6 +68,7 @@ exports.postSignup = (req, res, next) => {
 
   User.findOne({ email: email }).then(userDoc => {
     if (userDoc) {
+      req.flash('error', 'Email exists already. Please choose another one.');
       return res.redirect('/signup'); //user exists
     }
     return bcrypt
